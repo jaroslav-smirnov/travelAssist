dfdaf560cc38da348f6180ecebd076b69462e503
jaroslav-smirnov <rarryk@gmail.com>
2013-08-30 14:56:30 +0400

    some changes

---
+++ travelAssist/gen/ru/optimal_drive/receipt_calculator/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ru.optimal_drive.receipt_calculator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
